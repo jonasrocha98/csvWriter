fca35268d1eba8b403afb96abda97b0d3da4c0af
Jonas Rocha <jonasrochacursos@gmail.com>
2026-05-13 21:09:39 -0300

    feat: add sort by column to DataProcessor

--- src/main/java/org/jonasrocha98/processor/DataProcessor.java
+++ src/main/java/org/jonasrocha98/processor/DataProcessor.java
@@ -20,7 +20,20 @@ public class DataProcessor {
     }
 
     // ordenar linhas pelo valor de uma coluna
+    public static List<String[]> ordenarPorColuna(CsvData dados, int coluna){
+        List<String[]> resultado = new ArrayList<>(dados.getLinhas());
 
+        resultado.sort((linhaA, linhaB) -> {
+            try {
+                double numA = Double.parseDouble(linhaA[coluna]);
+                double numB = Double.parseDouble(linhaB[coluna]);
+                return Double.compare(numA, numB);
+            } catch (NumberFormatException e) {
+                return linhaA[coluna].compareTo(linhaB[coluna]);
+            }
+        });
+        return resultado;
+    }
 
 
 
